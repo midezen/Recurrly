cf0dad6c0820eb60ccc586033fcb94fa34d79f37
midezen <victordiya90@gmail.com>
2026-05-11 10:50:10 +0100

    implement fonts'

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,24 +6,22 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function Index() {
   return (
     <SafeAreaView className="flex-1 p-5 bg-background">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-5xl font-sans-extrabold">Home</Text>
       <Link
         href="/(auth)/sign-in"
-        className="mt-4 bg-black text-white p-4 rounded-md"
+        className="mt-4 font-sans-bold bg-black text-white p-4 rounded-md"
       >
         Sign in here
       </Link>
       <Link
         href="/(auth)/sign-up"
-        className="mt-4 bg-black text-white p-4 rounded-md"
+        className="mt-4 font-sans-bold bg-black text-white p-4 rounded-md"
       >
         Sign up here
       </Link>
       <Link
         href="/subscriptions/spotify"
-        className="mt-4 bg-black text-white p-4 rounded-md"
+        className="mt-4 font-sans-bold bg-black text-white p-4 rounded-md"
       >
         Sub details spotify
       </Link>
